834ce78c5042129ca88d2d84c344620e5d726af4
crushedhat <joe@deadnode.com>
2013-09-01 16:39:58 -0700

    Update
    Updated X-hypem to work with hypem.com's new code format

--- x-hypem.pl
+++ x-hypem.pl
@@ -5,6 +5,7 @@ use diagnostics;
 use JSON;
 use MP3::Tag;
 use LWP::UserAgent;
+use Data::Printer;
 require HTTP::Cookies;
 
 MP3::Tag->config(write_v24 => 1);
@@ -21,35 +22,39 @@ my $ua = LWP::UserAgent->new(
 $ua->cookie_jar(HTTP::Cookies->new(file => "cookies.txt", autosave => 1));		
 		
 my $response = $ua->get(_search($answer));
-my @source = split(/\n/, $response->decoded_content); 	#Initial page source is sucked into @shit as 1 line
-my @songinfo = ();											#Split seperates all indivdual lines of page source
+my @source = split(/\n/, $response->decoded_content); 
 
-
-foreach my $line (@source){											# every line in array
-	next if ($line !~ m/^\s*(id:|key: |artist:|song:)'(.*)',/); 		# ^ begining of line
-	push @songinfo,$2;														# s spaces (with * multiple)
-																		# () What you are searching for
-	if ($1 eq 'song:'){												#$1 first set of () - $2 second capture group						
-		my $file = _getsong($ua , \@songinfo);
+foreach my $line (@source) {
 		
-		my $mp3 = MP3::Tag->new("$file");
-		$mp3->update_tags( {
-			"title" => $songinfo[3],
-			"artist" => $songinfo[2],
-		});
-		@songinfo = ();				
-	}
-}											
-
-sub _getsong {																#[0] = id:
-	$ua = shift;															#[1] = key:
-	my $songinfo = shift;													#[2] = artist:
-	(my $unixtitle = $songinfo->[3]) =~ s/(\s|\/)/_/g;						#[3] = song:
+		next unless $line =~ m/^\s*\{"page_cur":/;
+		$line =~ s|</script>||;
+		
+		my $shit = decode_json($line);
+		
+		
+		foreach my $track ( @{ $shit->{tracks} }) {
+			my @songinfo = ();
+			push @songinfo,
+				$track->{id},
+				$track->{key},
+				$track->{artist},
+				$track->{song};
+				
+			 _getsong($ua, \@songinfo);
+		}
+		
+}		
+									
+sub _getsong {														
+	$ua = shift;															
+	my $songinfo = shift;	
+													
+	(my $unixtitle = $songinfo->[3]) =~ s/(\s|\/)/_/g;						
 	(my $unixartist = $songinfo->[2]) =~ s/(\s|\/)/_/g;
 	my $file = "/Users/havox/Desktop/Dubstep/$unixartist-$unixtitle.mp3";
 	
 	my $response = $ua->get("http://www.hypem.com/serve/source/$songinfo->[0]/$songinfo->[1]");
-	
+
 	unless (-e $file){		#unless file does not exist, download
 		$ua->mirror(decode_json($response->decoded_content)->{url} , $file);
 	}
@@ -61,7 +66,7 @@ sub _search {
 	my $url = "";
 	
 	if ($answer eq ""){
-		$url = "http://www.hypem.com/?ax=1\n";
+		$url = "http://www.hypem.com/popular?ax=1\n";
 	}
 	else{
 		$url = "http://www.hypem.com/search/$answer?ax=1\n";
